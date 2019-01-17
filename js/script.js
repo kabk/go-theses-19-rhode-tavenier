b4f9054815dbeebee09475d2ec565272be87f104
Rhode Tavenier <rmsn@live.nl>
2019-01-17 02:55:34 +0100

    thesis

---
+++ js/script.js
@@ -0,0 +1,386 @@
+$(function() {
+    $('#hover1').hover( function() { $('#hidden-box1').toggle(); } );
+});
+
+$(function() {
+    $('#hover2').hover( function() { $('#hidden-box2').toggle(); } );
+});
+
+$(function() { 
+    $('#hover3').hover( function() { $('#hidden-box3').toggle(); } );
+});
+
+$(function() { 
+    $('#hover4').hover( function() { $('#hidden-box4').toggle(); } );
+});
+
+$(function() { 
+    $('#hover5').hover( function() { $('#hidden-box5').toggle(); } );
+});
+
+$(function() { 
+    $('#hover6').hover( function() { $('#hidden-box6').toggle(); } );
+});
+
+$(function() { 
+    $('#hover7').hover( function() { $('#hidden-box7').toggle(); } );
+});
+
+$(function() { 
+    $('#hover8').hover( function() { $('#hidden-box8').toggle(); } );
+});
+
+$(function() { 
+    $('#hover9').hover( function() { $('#hidden-box9').toggle(); } );
+});
+
+$(function() { 
+    $('#hover10').hover( function() { $('#hidden-box10').toggle(); } );
+});
+
+$(function() { 
+    $('#hover11').hover( function() { $('#hidden-box11').toggle(); } );
+});
+
+$(function() { 
+    $('#hover12').hover( function() { $('#hidden-box12').toggle(); } );
+});
+
+$(function() { 
+    $('#hover13').hover( function() { $('#hidden-box13').toggle(); } );
+});
+
+$(function() { 
+    $('#hover14').hover( function() { $('#hidden-box14').toggle(); } );
+});
+
+$(function() { 
+    $('#hover15').hover( function() { $('#hidden-box15').toggle(); } );
+});
+
+$(function() { 
+    $('#hover16').hover( function() { $('#hidden-box16').toggle(); } );
+});
+
+$(function() { 
+    $('#hover17').hover( function() { $('#hidden-box17').toggle(); } );
+});
+
+$(function() { 
+    $('#hover18').hover( function() { $('#hidden-box18').toggle(); } );
+});
+
+$(function() { 
+    $('#hover19').hover( function() { $('#hidden-box19').toggle(); } );
+});
+
+$(function() { 
+    $('#hover20').hover( function() { $('#hidden-box20').toggle(); } );
+});
+
+$(function() { 
+    $('#hover21').hover( function() { $('#hidden-box21').toggle(); } );
+});
+
+$(function() { 
+    $('#hover22').hover( function() { $('#hidden-box22').toggle(); } );
+});
+
+$(function() { 
+    $('#hover23').hover( function() { $('#hidden-box23').toggle(); } );
+});
+
+$(function() { 
+    $('#hover24').hover( function() { $('#hidden-box24').toggle(); } );
+});
+
+$(function() { 
+    $('#hover25').hover( function() { $('#hidden-box25').toggle(); } );
+});
+
+$(function() { 
+    $('#hover26').hover( function() { $('#hidden-box26').toggle(); } );
+});
+
+$(function() { 
+    $('#hover27').hover( function() { $('#hidden-box27').toggle(); } );
+});
+
+$(function() { 
+    $('#hover28').hover( function() { $('#hidden-box28').toggle(); } );
+});
+
+$(function() { 
+    $('#hover29').hover( function() { $('#hidden-box29').toggle(); } );
+});
+
+
+$(function() { 
+    $('#hover30').hover( function() { $('#hidden-box30').toggle(); } );
+});
+
+$(function() { 
+    $('#hover31').hover( function() { $('#hidden-box31').toggle(); } );
+});
+
+$(function() { 
+    $('#hover32').hover( function() { $('#hidden-box32').toggle(); } );
+});
+
+$(function() { 
+    $('#hover33').hover( function() { $('#hidden-box33').toggle(); } );
+});
+
+$(function() { 
+    $('#hover34').hover( function() { $('#hidden-box34').toggle(); } );
+});
+
+$(function() { 
+    $('#hover35').hover( function() { $('#hidden-box35').toggle(); } );
+});
+
+
+$(".next1").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberTwo").offset().top - 40,
+        scrollLeft: $(".numberTwo").offset().left - $(".numberTwo").width() * 0.5
+    }, 3000);
+});
+
+$(".next2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberThree").offset().top - 40,
+        scrollLeft: $(".numberThree").offset().left - $(".numberThree").width() * 0.5
+    }, 4000);
+});
+
+$(".next3").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberFour").offset().top - 40,
+        scrollLeft: $(".numberFour").offset().left - $(".numberFour").width() * 0.5
+    }, 500);
+});
+
+$(".next4").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberFive").offset().top - 40,
+        scrollLeft: $(".numberFive").offset().left - $(".numberFive").width() * 0.5
+    }, 1000);
+});
+
+$(".next5").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberSix").offset().top - 40,
+        scrollLeft: $(".numberSix").offset().left - $(".numberSix").width() * 0.5
+    }, 2600);
+});
+
+$(".next6").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberSeven").offset().top - 40,
+        scrollLeft: $(".numberSeven").offset().left - $(".numberSeven").width() * 0.5
+    }, 2600);
+});
+
+$(".next7").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberEight").offset().top - 40,
+        scrollLeft: $(".numberEight").offset().left - $(".numberEight").width() * 0.5
+    }, 2600);
+});
+
+$(".next8").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberNine").offset().top - 40,
+        scrollLeft: $(".numberNine").offset().left - $(".numberNine").width() * 0.5
+    }, 2600);
+});
+
+
+$("#chapter1").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberTwo").offset().top - 40,
+        scrollLeft: $(".numberTwo").offset().left - $(".numberTwo").width() * 0.5
+    }, 100);
+});
+
+$("#chapter2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberThree").offset().top - 40,
+        scrollLeft: $(".numberThree").offset().left - $(".numberThree").width() * 0.5
+    }, 4000); // where you control the time to scroll
+});
+
+$("#chapter3").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberFour").offset().top - 40,
+        scrollLeft: $(".numberFour").offset().left - $(".numberFour").width() * 0.5
+    }, 1000); // where you control the time to scroll
+});
+
+$("#chapter4").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberFive").offset().top - 40,
+        scrollLeft: $(".numberFive").offset().left - $(".numberFive").width() * 0.5
+    }, 1000); // where you control the time to scroll
+});
+
+$("#chapter5").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberSix").offset().top - 40,
+        scrollLeft: $(".numberSix").offset().left - $(".numberSix").width() * 0.5
+    }, 1000); // where you control the time to scroll
+});
+
+$("#chapter6").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberSeven").offset().top - 40,
+        scrollLeft: $(".numberSeven").offset().left - $(".numberSeven").width() * 0.5
+    }, 1000); // where you control the time to scroll
+});
+
+$("#chapter7").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberEight").offset().top - 40,
+        scrollLeft: $(".numberEight").offset().left - $(".numberEight").width() * 0.5
+    }, 1000); // where you control the time to scroll
+});
+
+$("#chapter8").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".numberNine").offset().top - 40,
+        scrollLeft: $(".numberNine").offset().left - $(".numberNine").width() * 0.5
+    }, 1000); // where you control the time to scroll
+});
+
+//this is for box appearing and disappearing
+
+$(document).ready(function(){
+    $(".makeAppear1").click(function(){
+        $(".boxAppear1").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberOne").offset().top,
+            scrollLeft: $(".numberOne").offset().left
+        }, 1000);
+
+    });
+    $(".x1").click(function(){
+        $(".boxAppear1").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".makeAppear2").click(function(){
+        $(".boxAppear2").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberTwo").offset().top,
+            scrollLeft: $(".numberTwo").offset().left
+        }, 1000);
+
+    });
+    $(".x2").click(function(){
+        $(".boxAppear2").slideUp("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".makeAppear3").click(function(){
+        $(".boxAppear3").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberThree").offset().top,
+            scrollLeft: $(".numberThree").offset().left
+        }, 1000);
+
+    });
+    $(".x3").click(function(){
+        $(".boxAppear3").slideUp("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".makeAppear4").click(function(){
+        $(".boxAppear4").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberFour").offset().top,
+            scrollLeft: $(".numberFour").offset().left
+        }, 1000);
+
+    });
+    $(".x4").click(function(){
+        $(".boxAppear4").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".makeAppear5").click(function(){
+        $(".boxAppear5").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberFive").offset().top,
+            scrollLeft: $(".numberFive").offset().left
+        }, 1000);
+
+    });
+    $(".x5").click(function(){
+        $(".boxAppear5").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".makeAppear6").click(function(){
+        $(".boxAppear6").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberSix").offset().top,
+            scrollLeft: $(".numberSix").offset().left
+        }, 1000);
+
+    });
+    $(".x6").click(function(){
+        $(".boxAppear6").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".makeAppear7").click(function(){
+        $(".boxAppear7").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberSeven").offset().top,
+            scrollLeft: $(".numberSeven").offset().left
+        }, 1000);
+
+    });
+    $(".x7").click(function(){
+        $(".boxAppear7").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".makeAppear8").click(function(){
+        $(".boxAppear8").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberEight").offset().top,
+            scrollLeft: $(".numberEight").offset().left
+        }, 1000);
+
+    });
+    $(".x8").click(function(){
+        $(".boxAppear8").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".makeAppear9").click(function(){
+        $(".boxAppear9").slideDown("slow");
+        $('html,body').animate({
+            scrollTop: $(".numberNine").offset().top,
+            scrollLeft: $(".numberNine").offset().left
+        }, 1000);
+
+    });
+    $(".x9").click(function(){
+        $(".boxAppear9").slideUp("slow");
+    });
+});
+
+
